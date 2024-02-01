499171da447db3c392e2e6f17bca25de390ffbdc
brunoabneves <brunoabneves@hotmail.com>
2024-02-01 16:01:48 -0300

    Iniciando testes

---
+++ src/test/java/store/ojuara/produtoapi/service/ProdutoServiceImplTest.java
@@ -0,0 +1,32 @@
+package store.ojuara.produtoapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoServiceImplTest {
+
+    @Test
+    void visualizar() {
+    }
+
+    @Test
+    void visualizarPorUuid() {
+    }
+
+    @Test
+    void listar() {
+    }
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+}
