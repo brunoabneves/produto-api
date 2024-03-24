1116f09e3994b4a5a75d1aaf0cf43f9825d70f6e
brunoabneves <brunoabneves97@gmail.com>
2024-03-24 18:42:29 -0300

    Removendo codigo comentado

--- src/main/java/store/ojuara/produtoapi/ProdutoApiApplication.java
+++ src/main/java/store/ojuara/produtoapi/ProdutoApiApplication.java
@@ -5,15 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-//@OpenAPIDefinition(
-//		info = @Info(
-//				title = "Documentação da API",
-//				version = "1.0.0",
-//				description = "Exemplo de documentação Swagger para uma aplicação Spring Boot",
-//				contact = @Contact(name = "Nome do Contato", url = "https://exemplo.com", email = "contato@exemplo.com"),
-//				license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0")
-//		)
-//)
 public class ProdutoApiApplication {
 
 	public static void main(String[] args) {
